30a7c96b30d1331bb9787e376358e2ff95d5a4e7
Jaimie Hart <hartjme@gmail.com>
2022-02-22 17:00:29 -0500

    Started code

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+export const KandyKorner = () => {
+    return (
+        //fragment to put chilcdren under single component
+        //you invoke a function in React with <whatToInvoke />
+        <>
+            <h1>Kandy Korner</h1>
+        </>
+    )
+}
