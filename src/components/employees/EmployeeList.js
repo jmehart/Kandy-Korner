cbf64c6c27618aa78edbc0f1b15b563f9c2a6795
Jaimie Hart <hartjme@gmail.com>
2022-03-01 11:03:28 -0500

    Finished Main Chapters

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -5,33 +5,45 @@ import "./Employee.css"
 
 export const EmployeeList = () => {
     //export function
-    const [employees, changeEmployee] = useState([])
+    const [employees, setEmployee] = useState([])
+    const [totalEmployeeMessage, updateMessage]= useState("")
 
     const history = useHistory()
 
     useEffect(
-        //get data from API and pull it into application state of employees
         () => {
-            fetch("http://localhost:8089/employees")
-                .then(res => res.json())
-                .then((employeeAPIData) => {
-                    //you can not directly modify state in React - you always have to use the function that it provided you in useState
-                    //arguement is what you want the state to be
-                    changeEmployee(employeeAPIData)
-                })
-        },
-        []
+        fetch("http://localhost:8089/employees?_expand=location&_sort=locationId")
+            .then((response) => response.json())
+            .then((employees) => {
+                setEmployee(employees);
+            })
+    },
+    []
     )
-
-
-
+    useEffect(
+        () => {
+            updateMessage(`Kandy Korner has ${employees.length} employees`)
+        },
+        [employees]
+    )
+    const deleteEmployee = (id) => {
+        fetch(`http://localhost:8089/employees/${id}`, {
+            method: "DELETE"
+        })
+        .then(() => {
+            history.go("/employees")
+        })
+    }
 
     return (
         <>
             <h2>Employees</h2>
             <div className="hire_button">
-                <button className="btn btn-primary" onClick={() => history.push("/employee/form")}>Hire New Employee</button>
+                <button className="btn" onClick={() => history.push("/employee/form")}>Hire New Employee</button>
             </div>
+            {
+            <div>{totalEmployeeMessage}</div>
+            }
             {
                 employees.map(
                     (employeeObj) => {
@@ -39,10 +51,14 @@ export const EmployeeList = () => {
 
                             <div>
                                 <div><h4>{employeeObj.name}</h4></div>
-                                <div>{employeeObj.location}</div>
+                                <div>Location: {employeeObj.location.name}</div>
                                 <div>Is a Manager: {employeeObj.manager === true ? "yes" : "no"}</div>
                                 <div>Is Full time: {employeeObj.fullTime === true ? "yes" : "no"}</div>
                                 <div>Hourly rate: ${employeeObj.hourlyRate}.00</div>
+                                <div>Phone: {employeeObj.phone}</div>
+                            <div>
+                                <button className="btn-fire" onClick={() => { deleteEmployee(employeeObj.id) }}>Fire Employee</button>
+                            </div>
                             </div>
                         </section>
                     }
